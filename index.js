cedc34858f674a29f124411518fe78a407015169
“Keith <KeithRichardinTech@gmail.com>
2022-03-01 11:49:28 -0500

    backend up

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const { createUser, loginUser, getUsers } = require("./src/users");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/users", createUser);
+app.post("/users/login", loginUser);
+app.get("/users", getUsers);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
